21392812beee00c93c5c1bac10a4abb6cc4faceb
R. Garcia-Dias <rafaelagd@gmail.com>
2019-08-18 12:06:22 +0100

    Report created

--- src/data/exploration.py
+++ src/data/exploration.py
@@ -9,5 +9,8 @@ import pandas_profiling as pf
 # ======================================================================================================================
 PROJECT_PATH = getcwd()
 UFO_DATA = pd.read_csv('%s/data/complete.csv' % PROJECT_PATH, error_bad_lines=False)
+# ======================================================================================================================
+# Create report
+# ======================================================================================================================
 mkdir('%s/reports/' % PROJECT_PATH)
 pf.ProfileReport(UFO_DATA).to_file('%s/reports/ProfileReport.html' % PROJECT_PATH)
