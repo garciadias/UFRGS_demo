ddd00cc70153ffa425a8e8258b00581da0a43440
R. Garcia-Dias <rafaelagd@gmail.com>
2019-08-18 12:05:28 +0100

    Prepare the dataset and create data report

---
+++ src/data/data.sh
@@ -0,0 +1,4 @@
+# Prepare Dataset
+project_path=${PWD}
+unzip $project_path"/data/ufo-sightings.zip" -d $project_path"/data"
+rm $project_path"/data/ufo-sightings.zip"
---
+++ src/data/exploration.py
@@ -0,0 +1,13 @@
+"""Create report of the data"""
+from os import getcwd, mkdir
+
+import pandas as pd
+import pandas_profiling as pf
+
+# ======================================================================================================================
+# Load data
+# ======================================================================================================================
+PROJECT_PATH = getcwd()
+UFO_DATA = pd.read_csv('%s/data/complete.csv' % PROJECT_PATH, error_bad_lines=False)
+mkdir('%s/reports/' % PROJECT_PATH)
+pf.ProfileReport(UFO_DATA).to_file('%s/reports/ProfileReport.html' % PROJECT_PATH)
